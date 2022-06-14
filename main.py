7dcdd872e14559660bab065871ade37a36ef800a
chloe <csabijon@s6c.mlp.college>
2022-06-14 15:17:49 +0100

    did the icons

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template('index.html')
+
+@app.route("/courses")
+def courses():
+    return render_template('course.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
