a3cb8b9c73da9ec5c36fde1cba86b5b7c591b8fe
chloe <csabijon@s6c.mlp.college>
2022-06-14 10:24:32 +0100

    ha

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def base():
-    return render_template("index.html")
-
+def hello_world():
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
