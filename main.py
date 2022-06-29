8681e264d92f25146a4ae7e65be06a79acfb0fd8
chloe <csabijon@s6c.mlp.college>
2022-06-29 11:20:25 +0100

    .

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from requests import request
 
 app = Flask(__name__)
 
@@ -13,13 +14,15 @@ def courses():
     return render_template('course.html')
 
 
+
 @app.route("/about-us")
 def about_us():
     return render_template('about-us.html')
 
 @app.route("/applied-science")
 def applied():
-    return render_template('applied.html')
+
+    return render_template('applied.html', description="")
 
 
 
